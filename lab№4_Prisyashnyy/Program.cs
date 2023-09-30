813d64b54d13e3c67e7cdb8d387601ccade16fcb
prisy <prisy@LAPTOP-23R98U4U>
2023-09-30 12:09:14 +0400

    Добавьте файлы проекта.

---
+++ lab№4_Prisyashnyy/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y: ");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine(((Math.Pow(x - 0, 2) + Math.Pow(y - 0, 2)) <= (Math.Pow(2, 2))) && ((Math.Pow(x - 0, 2) + Math.Pow(y - 0, 2)) >= (Math.Pow(1, 2))));
